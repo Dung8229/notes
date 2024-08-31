33600d533915de3182bfa460212845d485bd9606
Dũng Quang <22028229@vnu.edu.vn>
2024-08-31 10:28:16 +0700

    Fix

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,5 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
-
-app.use(cors)
 
 let notes = [
   {
@@ -92,7 +89,7 @@ app.delete('/api/notes/:id', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
